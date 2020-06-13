b0c969771df1114ca8da760b423cb75bd43ffe9f
dhruvkelawala <dhruvrk2000@gmail.com>
2020-06-13 23:06:07 +0530

    Define the variable and then assign it value

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-
 function updateMap() {
-    fetch("/data.json")
-        .then(response => response.json())
-        .then(rsp => {
-            console.log(rsp.records);
-            rsp.records.forEach(element => {
-                latitude = element.latitude;
-                longitude = element.longitude;
+  fetch('./data.json')
+    .then((response) => response.json())
+    .then((rsp) => {
+      console.log(rsp.records);
+      rsp.records.forEach((element) => {
+        const latitude = element.latitude;
+        const longitude = element.longitude;
 
-                new mapboxgl.Marker({
-                    draggable: true
-                })
-                    .setLngLat([longitude, latitude])
-                    .addTo(map);
-
-            });
+        new mapboxgl.Marker({
+          draggable: true,
         })
+          .setLngLat([longitude, latitude])
+          .addTo(map);
+      });
+    });
 }
 
 updateMap();
